a40153518e4862fecbe0dc2b90be73f90a15ccd0
Harsh  kori <141260570+harsh7editor@users.noreply.github.com>
2024-06-07 18:54:04 +0530

    ANIMATED + WEB

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["JAVA Developer . . ." , "VIDEO EDITOR. . ." , "Web Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
